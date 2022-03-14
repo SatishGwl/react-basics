5b1f8ab56269c87c32a660ea536cef482b0055a1
satishsharma <satishs@dewsolutions.in>
2022-03-14 23:42:23 +0530

    add event and event Listner

--- src/component/Subscription.js
+++ src/component/Subscription.js
@@ -2,13 +2,18 @@ import React from "react";
 import "./Subscription.css";
 import SubscriptionDate from "./SubscriptionDate";
 
-function Subscription(props) {
-  
+const Subscription = (props) => {
+  let title = props.title
+  const changeTitle = (e) => {
+    title = 'change Title wrap'
+    console.log('onclick button')
+  }
   return (
     <div className="subscription">
         <SubscriptionDate date={props.date}/>
-      <h2 className="subscription_title">{props.title}</h2>
+      <h2 className="subscription_title">{title}</h2>
       <div className="subscription_price"> {props.amount} </div>
+      <button type="button" id="changeBtnTitle" onClick={changeTitle}>Change Title</button>
     </div>
   );
 }
